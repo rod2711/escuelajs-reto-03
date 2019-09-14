28119924de05efaa30a2cac3deba5a7576b281bd
Rodrigo Ortiz <rodrigortizm@gmail.com>
2019-09-14 11:17:50 -0500

    reto paso 1 resulto

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ var xhttp = new XMLHttpRequest();
 
 function fetchData(url_api, callback) {
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200)
+      
+        callback(null, JSON.parse(xhttp.responseText));
       else return callback(url_api);
     }
   };
@@ -16,11 +17,12 @@ function fetchData(url_api, callback) {
 };
 
 fetchData(API, function (error1, data1) {
+  console.log('entered Callback');
   if (error1) return console.error('Error' + ' ' + error1);
   console.log('Primer Llamado...')
   fetchData(API + data1.results[0].id, function (error2, data2) {
     if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
+    console.log('Segundo Llamado...');
     fetchData(data2.origin.url, function (error3, data3) {
       if (error3) return console.error(error3);
       console.log('Tercero Llamado...')
